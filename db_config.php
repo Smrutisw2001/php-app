61aa2c4457e5bbd07a14b5015f99f1bc418ccb56
Smruti Ranjan <Kanhu@2001>
2024-09-20 05:04:13 +0000

    php_update

--- db_config.php
+++ db_config.php
@@ -1,9 +1,9 @@
 <?php
 // Database configuration
-$servername = "mysqldb.c70ikgow8mn5.ap-south-1.rds.amazonaws.com";  // Replace with your RDS endpoint
-$username = "admin";     // Your DB username
-$password = "Kanhu2001";     // Your DB password
-$dbname = "mysqldb";              // Your DB name
+$servername = "your rds server endpoint";  // Replace with your RDS endpoint
+$username = "user name";     // Your DB username
+$password = "passwd";     // Your DB password
+$dbname = "database name";              // Your DB name
 
 // Create a connection
 $conn = new mysqli($servername, $username, $password, $dbname);
